9ca690064ecb2ee2fd98f068ebb64c54fa5bedc7
Harliin <56713048+Harliin@users.noreply.github.com>
2020-01-09 11:58:13 +0100

    rest

---
+++ DB-Kockterminal/DB-Kockterminal/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB_Kockterminal
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
---
+++ DB-Utlämning/DB-Utlämning/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB_Utlämning
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
