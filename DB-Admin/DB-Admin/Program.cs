3619c782264f7d191f0cfff93ed18afd5d871eb5
Harliin <56713048+Harliin@users.noreply.github.com>
2020-01-10 10:47:44 +0100

    test2

--- DB-Admin/DB-Admin/Program.cs
+++ DB-Admin/DB-Admin/Program.cs
@@ -6,7 +6,7 @@ namespace DB_Admin
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("123");
         }
     }
 }
