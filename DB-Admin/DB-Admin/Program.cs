98ef38cd96b0e2a8c441c552561d084e05e6720d
Harliin <56713048+Harliin@users.noreply.github.com>
2020-01-10 10:43:42 +0100

    nya

---
+++ DB-Admin/DB-Admin/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace DB_Admin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
