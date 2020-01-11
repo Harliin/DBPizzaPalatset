cd6838883ac60a363c14642f2bc7d831755db96a
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2020-01-11 16:13:08 +0100

    Kopierade kod från Grupp 1. Info
    Den inte än färdig. Programmet fungerar ej korrekt.

---
+++ DB-OrderInfo/DB-OrderInfo/Customer.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB_OrderInfo
+{
+    public class Customer
+    {
+        public int ID { get; set; }
+        private static int IDCounter = 1;
+
+        public Customer()
+        {
+            ID = IDCounter;
+            IDCounter++;
+        }
+    }
+}
---
+++ DB-OrderInfo/DB-OrderInfo/OrderOngoings.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+
+namespace DB_OrderInfo
+{
+    /*Denna klass hanterar de pågående ordrarna,
+      den kan lägga till, ta bort samt skriva ut pågående ordrar*/
+    public class OrdersOngoing
+    {
+        public List<Customer> OngoingOrderID;
+
+
+        public OrdersOngoing()
+        {
+            this.OngoingOrderID = new List<Customer>();
+        }
+
+        public void ShowOngoingOrders()/*Skriver ut pågående ordrar*/
+        {
+
+            foreach (Customer customer in OngoingOrderID)
+            {
+                Console.WriteLine($"#ID: {customer.ID}");
+            }
+        }
+
+        public void NewOrder() /*Skapar en ny order och lägger till den i Listan för ordrar*/
+        {
+            Customer customer = new Customer();
+            if (customer.ID % 2 == 0)
+                Thread.Sleep(1000);
+            OngoingOrderID.Add(customer);
+        }
+
+        public Customer FinishedOrder()/*Flyttar över en pågående order till färdig order.
+                                        Samt tar bort den pågående ordern*/
+        {
+            Customer customer = OngoingOrderID[0];
+            OngoingOrderID.Remove(OngoingOrderID[0]);
+            return customer;
+        }
+
+
+    }
+}
