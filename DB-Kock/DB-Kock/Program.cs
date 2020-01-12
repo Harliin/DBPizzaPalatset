90b927503b89362aab89a113d8daa66c1295b815
AataMie <57095052+AataMie@users.noreply.github.com>
2020-01-12 15:22:02 +0100

    123

--- DB-Kock/DB-Kock/Program.cs
+++ DB-Kock/DB-Kock/Program.cs
@@ -6,7 +6,7 @@ namespace DB_Kock
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("123");
         }
     }
 }
