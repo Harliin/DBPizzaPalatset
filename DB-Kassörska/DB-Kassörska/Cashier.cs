7fe7839e695a1e7a3d3242d4facbec90d7b33d68
thomascarleberg <thomas_carlsson@hotmail.com>
2020-01-14 13:50:45 +0100

    Uppdaterat koden

---
+++ DB-Kassörska/DB-Kassörska/Cashier.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB_Kassörska
+{
+    class Cashier
+    {
+        public void CashierManagement()
+        {
+            Console.Clear();
+            Console.WriteLine("--Kassörterminal--");
+
+            Console.WriteLine("----------------------------------");
+            getCurrentOrders();
+            getCompleteOrders();
+            Console.WriteLine("----------------------------------");
+            Console.WriteLine("[1] Markera order som uthämtad");
+
+            char cashierChoice = Console.ReadKey(true).KeyChar;
+
+            switch (cashierChoice)
+            {
+                case '1':
+                    markOrderAsCollected();
+                    Console.ReadKey();
+                    CashierManagement();
+                    break;
+
+                default:
+                    Console.WriteLine("Fel inmatning!");
+                    Console.ReadKey(true);
+                    CashierManagement();
+                    break;
+            }
+        }
+
+        public static void getCurrentOrders()
+        {
+            Console.WriteLine("Pågående ordrar: Hämtar från DB...");
+        }
+
+        public static void getCompleteOrders()
+        {
+            Console.WriteLine("Färdiga ordrar: Hämtar från DB...");
+        }
+
+        public static void markOrderAsCollected()
+        {
+            Console.WriteLine("Markerar order som uthämtad i DB...");
+        }
+
+    }
+
+
+}
--- DB-Kassörska/DB-Kassörska/Program.cs
+++ DB-Kassörska/DB-Kassörska/Program.cs
@@ -6,26 +6,10 @@ namespace DB_Kassörska
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Kassörterminal");
-            getCurrentOrders();
-            getCompleteOrders();
-            Console.WriteLine("Tryck 1 för att markera order som uthämtad");
+            Cashier cashier = new Cashier();
 
-        }
+            cashier.CashierManagement();
 
-        public static void getCurrentOrders()
-        {
-            Console.WriteLine("Hämtar pågående ordrar från databasen...");
-        }
-
-        public static void getCompleteOrders()
-        {
-            Console.WriteLine("Hämtar klara ordrar från databasen...");
-        }
-
-        public static void markOrderAsComplete()
-        {
-            Console.WriteLine("Flyttar order till listan över klara ordrar");
         }
     }
 }
