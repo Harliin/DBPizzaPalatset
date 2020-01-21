48f5e3488d61cc38a3cc628bfd66f4e96a781545
thomascarleberg <thomas_carlsson@hotmail.com>
2020-01-21 12:44:32 +0100

    Create Order.cs

---
+++ DB-Kassörska/DB-Kassörska/Food/Order.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DB_Kassörska.Food
+{
+    class Order
+    {
+        public int ID { get; set; }
+        public enum Status { Pågående = 1, Klar = 2 }
+
+    }
+}
