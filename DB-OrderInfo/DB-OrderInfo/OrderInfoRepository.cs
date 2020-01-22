1724cf0d7a18b50722e6e601a35acda94ada5dd2
Hitman08 <56691277+Hitman08@users.noreply.github.com>
2020-01-22 12:10:18 +0100

    Programmet körs
    Adderade connection och anrop från DB

---
+++ DB-OrderInfo/DB-OrderInfo/OrderInfoRepository.cs
@@ -0,0 +1,37 @@
+﻿using Dapper;
+using DB_OrderInfo.Food;
+using Food;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Threading.Tasks;
+
+namespace DB_OrderInfo
+{
+    class OrderInfoRepository
+    {
+        private string ConnectionString { get; }
+        private SqlConnection Connection { get; }
+        public OrderInfoRepository()
+        {
+            ConnectionString = "Data Source=SQL6009.site4now.net;Initial Catalog=DB_A53DDD_Grupp1;User Id=DB_A53DDD_Grupp1_admin;Password=Password123;";
+            Connection = new SqlConnection(ConnectionString);
+            Connection.Open();
+        }
+        public async Task<IEnumerable<Pizza>> Pizzas()
+        {
+            IEnumerable<Pizza> pizzas = (await Connection.QueryAsync<Pizza>("GetPizzas", commandType: CommandType.StoredProcedure));
+            return pizzas;
+        }
+        public async Task<IEnumerable<Order>> OngoingOrder()
+        {
+            IEnumerable<Order> ongoingOrder = (await Connection.QueryAsync<Order>("GetOngoingOrders", commandType: CommandType.StoredProcedure));
+            return ongoingOrder;
+        }
+        public async Task<IEnumerable<Order>> CompleteOrder()
+        {
+            IEnumerable<Order> completeOrder = (await Connection.QueryAsync<Order>("GetFinishedOrders", commandType: CommandType.StoredProcedure));
+            return completeOrder;
+        }
+    }
+}
--- DB-OrderInfo/DB-OrderInfo/Program.cs
+++ DB-OrderInfo/DB-OrderInfo/Program.cs
@@ -1,11 +1,32 @@
-﻿namespace DB_OrderInfo
+﻿using DB_OrderInfo.Food;
+using System;
+using System.Threading.Tasks;
+
+namespace DB_OrderInfo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
-            MainMenu menu = new MainMenu();
-            menu.Run();
+            Console.WriteLine("Välkommen till PIZZERIA PALATSET !\n");
+            Console.WriteLine("Pågående ordrar:\n");
+            OrderInfoRepository repository = new OrderInfoRepository();
+
+            //foreach (Pizza pizzas in await repository.Pizzas())
+            //{
+            //    Console.WriteLine($"{pizzas.Name}");
+            //}
+
+            foreach (Order ongoingOrder in await repository.OngoingOrder())
+            {
+                Console.WriteLine(ongoingOrder.ID);
+            }
+
+            Console.WriteLine("\nFärdiga ordrar:\n");
+            foreach (Order completeOrder in await repository.CompleteOrder())
+            {
+                Console.WriteLine(completeOrder.ID);
+            }
         }
     }
 }
