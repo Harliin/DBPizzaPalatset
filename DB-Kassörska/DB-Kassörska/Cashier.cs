9224f943ccdefd25870b91de388c0cee71b25f37
thomascarleberg <thomas_carlsson@hotmail.com>
2020-01-31 11:31:10 +0100

    ändringar

--- DB-Kassörska/DB-Kassörska/Cashier.cs
+++ DB-Kassörska/DB-Kassörska/Cashier.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace DB_Kassörska
@@ -11,23 +12,21 @@ namespace DB_Kassörska
         /*TODO
          * Felhantering om man skriver in fel nummer, bokstav osv. While (correctKey == false) osv
          */
+
+        CashierRepository repo = new CashierRepository();
         public int correctKey { get; set; }
-        public async Task CashierManagement()
+        public async Task CashierManagement() //Kassörskans meny
         {
-            CashierRepository repo = new CashierRepository();
-
-            Console.Clear();
-
-            
-            Console.WriteLine("--Kassörterminal--");
-
-            Console.WriteLine("Alla ordrar:");
-            Console.WriteLine("-----------------------------------");
-
             var orders = await repo.ShowAllOrdersAsync();
             List<Order> orderList = orders.ToList();
 
-            foreach (var order in await repo.ShowAllOrdersAsync())
+            Console.Clear();
+
+            Console.WriteLine("--Kassörterminal--");
+            Console.WriteLine("Alla ordrar:");
+            Console.WriteLine("-----------------------------------");
+
+            foreach (var order in orders)
             {
                 Console.WriteLine($"Order-ID: {order.ID} Orderstatus: {order.Status}");
             }
@@ -41,19 +40,22 @@ namespace DB_Kassörska
                 Console.WriteLine($"Order-ID: {order.ID} Orderstatus: {order.Status}");
             }
 
-            Console.SetCursorPosition(67, 25);
+            Console.Write("Markera order som uthämtad (ange ordernummer och tryck enter): ");
 
-            Console.WriteLine("Markera order som uthämtad (ange ordernummer och tryck enter)");
-
-            if(int.TryParse(Console.ReadLine(), out int cashierOrderChoice))
+            if (int.TryParse(Console.ReadLine(), out int cashierOrderChoice))
             {
-                if(orderList.Exists(x => x.ID == cashierOrderChoice))
+                if (orderList.Exists(x => x.ID == cashierOrderChoice))
                 {
                     await repo.UpdateOrderStatus(cashierOrderChoice);
+                    Console.WriteLine($"Markerar order {cashierOrderChoice} som uthämtad");
+                    Thread.Sleep(2000);
+                    await CashierManagement();
                 }
                 else
                 {
                     Console.WriteLine("Ordernumret finns inte!");
+                    Thread.Sleep(2000);
+                    await CashierManagement();
                 }
             }
         }
--- DB-Kassörska/DB-Kassörska/Repo/CashierRepository.cs
+++ DB-Kassörska/DB-Kassörska/Repo/CashierRepository.cs
@@ -20,7 +20,7 @@ namespace DB_Kassörska
             connection = new SqlConnection(ConnectionString);
             connection.Open();
         }
-        public async Task UpdateOrderStatus(int orderNumber)
+        public async Task UpdateOrderStatus(int orderNumber) //Uppdatera orderns status
         {
             await connection.QueryAsync<Pizza>("UpdateOrderStatus",
                 new { @ID = orderNumber }, commandType: CommandType.StoredProcedure);
@@ -30,58 +30,58 @@ namespace DB_Kassörska
             IEnumerable<Order> allOrders = (await connection.QueryAsync<Order>("ShowOrders", commandType: CommandType.StoredProcedure));
 
             return allOrders;
-        }
+        } //Visa alla ordrar
 
-        public async Task<IEnumerable<Order>> ShowOrderByIDAsync(int orderNumber)
+        public async Task<IEnumerable<Order>> ShowOrderByIDAsync(int orderNumber) //Visa ordrar baserat på ID
         {
             IEnumerable<Order> ongoingOrders = (await connection.QueryAsync<Order>("ShowOrderByID", new { @ID = orderNumber }, commandType: CommandType.StoredProcedure));
 
             return ongoingOrders;
         }
 
-        public async Task AddPizzaAsync(string name, int price)
+        public async Task AddPizzaAsync(string name, int price) //Lägg till pizza
         {
             await connection.QueryAsync<Pizza>("AddPizza",
                 new { Name = name, Price = price }, commandType: CommandType.StoredProcedure);
         }
-        public async Task AddIngredientToPizzaAsync(int pizzaID, int[] ingridients)
+        public async Task AddIngredientToPizzaAsync(int pizzaID, int[] ingridients) //Lägg till ingrediens till pizza
         {
             foreach (var ingredient in ingridients)
             {
                 await connection.QueryAsync<Pizza>("INSERT INTO PizzaIngredients(PizzaID, IngredientsID) VALUES (@PizzaID, @IngredientID)", new { PizzaID = pizzaID, IngredientID = ingredient });
             }
         }
-        public async Task<IEnumerable<PizzaIngredient>> ShowPizzaAndIngredients()
+        public async Task<IEnumerable<PizzaIngredient>> ShowPizzaAndIngredients() //Visa pizzans ingredienser
         {
             IEnumerable<PizzaIngredient> pizzaIngredients = await connection.QueryAsync<PizzaIngredient>("ShowPizzaIngredients", commandType: CommandType.StoredProcedure);
             return pizzaIngredients;
         }
-        public async Task<IEnumerable<Pizza>> ShowPizzasAsync()
+        public async Task<IEnumerable<Pizza>> ShowPizzasAsync() //Visa vilka pizzor som finns att köpa
         {
             IEnumerable<Pizza> pizzas = await connection.QueryAsync<Pizza>("GetPizzas", commandType: CommandType.StoredProcedure);
             return pizzas;
         }
-        public async Task<IEnumerable<Ingredient>> ShowIngredientsAsync()
+        public async Task<IEnumerable<Ingredient>> ShowIngredientsAsync() //Visa vilka ingredienser som finns att använda
         {
             IEnumerable<Ingredient> ingredients = await connection.QueryAsync<Ingredient>("GetIngredients", commandType: CommandType.StoredProcedure);
             return ingredients;
         }
-        public async Task<IEnumerable<Pasta>> ShowPastasAsync()
+        public async Task<IEnumerable<Pasta>> ShowPastasAsync() //Visa vilka pastor som finns att köpa
         {
             IEnumerable<Pasta> pastas = await connection.QueryAsync<Pasta>("GetPastas", commandType: CommandType.StoredProcedure);
             return pastas;
         }
-        public async Task<IEnumerable<Sallad>> ShowSalladsAsync()
+        public async Task<IEnumerable<Sallad>> ShowSalladsAsync() //Visa vilka sallader som finns att köpa
         {
             IEnumerable<Sallad> sallads = await connection.QueryAsync<Sallad>("GetSallads", commandType: CommandType.StoredProcedure);
             return sallads;
         }
-        public async Task<IEnumerable<Drink>> ShowDrinksAsync()
+        public async Task<IEnumerable<Drink>> ShowDrinksAsync() //Visa vilka drycker som finns att köpa
         {
             IEnumerable<Drink> drinks = await connection.QueryAsync<Drink>("GetDrinks", commandType: CommandType.StoredProcedure);
             return drinks;
         }
-        public async Task<IEnumerable<Extra>> ShowExtraAsync()
+        public async Task<IEnumerable<Extra>> ShowExtraAsync() //Visa vilka tillägg som finns att köpa
         {
             IEnumerable<Extra> drinks = await connection.QueryAsync<Extra>("GetExtras", commandType: CommandType.StoredProcedure);
             return drinks;
