d340bd02da8ed1aa245135e42c0f4211f5a2b27f
thomascarleberg <thomas_carlsson@hotmail.com>
2020-02-07 16:55:48 +0100

    ändringar

--- DB-Kassörska/DB-Kassörska/Cashier.cs
+++ DB-Kassörska/DB-Kassörska/Cashier.cs
@@ -71,5 +71,40 @@ namespace DB_Kassörska
                 await CashierManagement();
             }
         }
+
+        public async Task ChooseBackend()//Väljer backend mellan MSSQL och PostgreSQL
+        {
+            CashierRepository repo;
+            Console.Clear();
+            Console.WriteLine("[1]MSSQL\n[2]PostgreSQL");
+            Console.Write("Välj Backend: ");
+            if (int.TryParse(Console.ReadLine(), out int backend))
+            {
+                if (backend == 1)//MSSQL
+                {
+                    CashierRepository.Backend = backend;
+                    repo = new CashierRepository();
+                    return;
+                }
+                else if (backend == 2)//PostgreSQL
+                {
+                    CashierRepository.Backend = backend;
+                    repo = new CashierRepository();
+                    return;
+                }
+                else
+                {
+                    Console.WriteLine("Ange en korrekt siffra!");
+                    Console.ReadKey(true);
+                    await ChooseBackend();
+                }
+            }
+            else
+            {
+                Console.WriteLine("Fel inmatat!");
+                Console.ReadKey(true);
+                await ChooseBackend();
+            }
+        }
     }
 }
--- DB-Kassörska/DB-Kassörska/Program.cs
+++ DB-Kassörska/DB-Kassörska/Program.cs
@@ -11,42 +11,7 @@ namespace DB_Kassörska
         {
             Cashier cashier = new Cashier();
 
-            await cashier.CashierManagement();
-        }
-
-        private static async Task ChooseBackend()//Väljer backend mellan MSSQL och PostgreSQL
-        {
-            CashierRepository repo;
-            Console.Clear();
-            Console.WriteLine("[1]MSSQL\n[2]PostgreSQL");
-            Console.Write("Välj Backend: ");
-            if (int.TryParse(Console.ReadLine(), out int backend))
-            {
-                if (backend == 1)//MSSQL
-                {
-                    CashierRepository.Backend = backend;
-                    repo = new CashierRepository();
-                    return;
-                }
-                else if (backend == 2)//PostgreSQL
-                {
-                    CashierRepository.Backend = backend;
-                    repo = new CashierRepository();
-                    return;
-                }
-                else
-                {
-                    Console.WriteLine("Ange en korrekt siffra!");
-                    Console.ReadKey(true);
-                    await ChooseBackend();
-                }
-            }
-            else
-            {
-                Console.WriteLine("Fel inmatat!");
-                Console.ReadKey(true);
-                await ChooseBackend();
-            }
+            await cashier.ChooseBackend();
         }
     }
 }
